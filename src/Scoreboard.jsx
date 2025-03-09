cab38f32b88815b10166809f7b728d3f3a6ce325
Mike D'Orazio <mike.dorazio@pepsico.com>
2025-03-08 22:49:23 -0500

    Updated Scorecard with trophy for winner

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,9 +1,33 @@
 export default function Scoreboard( { score, currentPlayer }) {
 
-    const whitePointerString = currentPlayer.code == 1 ? String.fromCodePoint('0x1f449') : String.fromCodePoint('0xe0020');
-    const blackPointerString = currentPlayer.code == 2 ? String.fromCodePoint('0x1f449') : String.fromCodePoint('0xe0020');
-    
-    console.log("Scoreboard.currentPlayer", currentPlayer, whitePointerString, blackPointerString);
+    let whitePointerString;
+    let blackPointerString;
+    let totalScore = score[0] + score[1];
+
+    if (totalScore == 64) {
+        // white wins
+        if (score[0] > score[1]) {
+            whitePointerString = String.fromCodePoint('0x1f3c6');
+            blackPointerString = String.fromCodePoint('0xe0020');
+        }
+        // black wins
+        else if (score[1] > score[0]) {
+            blackPointerString = String.fromCodePoint('0x1f3c6');
+            whitePointerString = String.fromCodePoint('0xe0020');
+        }
+        // tie
+        else {
+            whitePointerString = String.fromCodePoint('0x1f3c6');
+            blackPointerString = String.fromCodePoint('0x1f3c6');
+        }
+    }
+    // game is not over...whose turn is it?
+    else {
+        whitePointerString = currentPlayer.code == 1 ? String.fromCodePoint('0x1f449') : String.fromCodePoint('0xe0020');
+        blackPointerString = currentPlayer.code == 2 ? String.fromCodePoint('0x1f449') : String.fromCodePoint('0xe0020');
+    }
+
+    console.log("Scoreboard.currentPlayer", totalScore, currentPlayer, whitePointerString, blackPointerString);
     return (
         <div className="score-board">
             <span>Scoreboard</span>
